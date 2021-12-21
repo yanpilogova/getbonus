5820576ae615104a9e01e30be231644416ffd8e5
yanpilogova <anpilogova1986@gmail.com>
2021-12-21 14:16:11 +0700

    added files

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 100;
+        int transfer = 1100;
+        int bonus;
+            if (transfer >= 1000) {
+            bonus = transfer/100;
+        } else {
+                bonus = 0;
+        }
+        int total;
+                total = currentBalance + transfer + bonus;
+                System.out.println("Общая сумма = " + total + " руб");
+        System.out.println("Количество бонусных рублей = " + bonus);
+        }
+    }
