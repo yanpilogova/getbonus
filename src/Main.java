436f2dec7267d22eb0e572fc2024500d614de9ad
yanpilogova <anpilogova1986@gmail.com>
2021-12-26 11:21:42 +0700

    modified the code style

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
         int currentBalance = 100;
         int transfer = 1100;
         int bonus;
-            if (transfer >= 1000) {
-            bonus = transfer/100;
+        if (transfer >= 1000) {
+            bonus = transfer / 100;
         } else {
-                bonus = 0;
+            bonus = 0;
         }
         int total;
-                total = currentBalance + transfer + bonus;
-                System.out.println("Общая сумма = " + total + " руб");
+        total = currentBalance + transfer + bonus;
+        System.out.println("Общая сумма = " + total + " руб");
         System.out.println("Количество бонусных рублей = " + bonus);
-        }
     }
+}
